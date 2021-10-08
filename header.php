8dd02dd0fd7d14e4f6ccd82440b72a873290e76c
Joebil Lotayco <joebil.lotayco@gmail.com>
2021-10-08 10:01:55 +0800

    Update

--- header.php
+++ header.php
@@ -5,8 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7791350815942001"
-        crossorigin="anonymous"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-TL84ETBFRX"></script>
     <script>
